f73a4d6c898dc5cf2519031d4de497b2d3c63e71
Mohamedshaban96 <mohamedshaban1234@gmail.com>
2023-10-18 19:21:08 -0700

    adding prompt

---
+++ prompt.c
@@ -0,0 +1,11 @@
+#include "shell.h"
+/**
+ * prompt_check - handling prompt on
+ * interactive/non-interactive mode
+ * Return: nothing
+ */
+void prompt_check(void)
+{
+	if (isatty(STDIN_FILENO))
+		write(1, "#cisfun$ ", 9);
+}
